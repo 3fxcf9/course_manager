77d60498afcc3843dc62a90fc657f404a564d734
3fxcf9 <123954477+3fxcf9@users.noreply.github.com>
2025-08-07 21:25:38 +0200

    Chapter list

--- src/course_manager.v
+++ src/course_manager.v
@@ -11,8 +11,8 @@ pub struct Context {
 
 pub struct App {
 	veb.StaticHandler
+	root      string
 	root_meta RootMeta
-	subjects  []SubjectMeta
 }
 
 fn main() {
@@ -59,6 +59,13 @@ mut:
 	path  string  @[json: '-']
 }
 
+struct ChapterMeta {
+	name  string
+	date  string
+	cover ?string
+	path  string
+}
+
 fn parse_subjects(root_path string) ![]SubjectMeta {
 	directories := (os.ls(root_path) or { [] }).filter(os.is_dir(os.join_path(root_path,
 		it)))
@@ -78,6 +85,27 @@ fn parse_subjects(root_path string) ![]SubjectMeta {
 	return subjects
 }
 
+fn parse_chapters(root_path string, subject_path string) ![]ChapterMeta {
+	directories := (os.ls(os.join_path_single(root_path, subject_path)) or { [] }).filter(os.is_dir(os.join_path(root_path,
+		subject_path, it)))
+
+	mut chapters := []ChapterMeta{}
+	for dir in directories {
+		chapters << ChapterMeta{
+			name:  os.join_path_single(subject_path, dir).all_after_last('/') // TODO: Use md header as title
+			date:  'FA/KE/DATE' // TODO: Use md date (add to md parser)
+			cover: if os.exists(os.join_path(root_path, subject_path, dir, 'cover.svg')) {
+				'cover.svg'
+			} else {
+				none
+			}
+			path:  os.join_path_single(subject_path, dir)
+		}
+	}
+
+	return chapters
+}
+
 fn web(cmd Command) ! {
 	figure_folder_path := cmd.args[0]
 
@@ -90,13 +118,10 @@ fn web(cmd Command) ! {
 	info_json := os.read_file(meta_file_path)!
 	root_meta := json.decode(RootMeta, info_json)!
 
-	// Sujbect list
-	subjects := parse_subjects(figure_folder_path)!
-
 	// Start the web server
 	mut app := &App{
+		root:      figure_folder_path
 		root_meta: root_meta
-		subjects:  subjects
 	}
 	app.static_mime_types['.md'] = 'txt/plain'
 	app.static_mime_types['.mde'] = 'txt/plain'
@@ -104,6 +129,14 @@ fn web(cmd Command) ! {
 	veb.run[App, Context](mut app, 8081)
 }
 
+@['/']
 pub fn (app &App) index(mut ctx Context) veb.Result {
+	subjects := parse_subjects(app.root) or { return ctx.request_error('Error') }
+	return $veb.html()
+}
+
+@['/subject/:subject_name']
+pub fn (app &App) subject(mut ctx Context, subject_name string) veb.Result {
+	chapters := parse_chapters(app.root, subject_name) or { return ctx.request_error('Error') }
 	return $veb.html()
 }
